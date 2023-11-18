82a96e243550d5af8c8278b1329c36d1067c2daf
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-11-18 06:51:31 +0530

    Delete unnecessary image files and update layout and stylesheets

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('pages/user/user-homePage', { admin:false });
 });
 
+router.get('/about/visionaries', function(req, res, next) {
+  res.render('pages/user/visionaries', { admin:false });
+});
+
+router.get('/academics', function(req, res, next) {
+  res.render('pages/user/academics', { admin:false });
+});
+
+router.get('/offices', function(req, res, next) {
+  res.render('pages/user/offices', { admin:false });
+});
+
+router.get('/unions/lisan', function(req, res, next) {
+  res.render('pages/user/unions', { admin:false, lisan:true, dsc:false });
+});
+
+router.get('/unions/dsc', function(req, res, next) {
+  res.render('pages/user/unions', { admin:false, lisan:false, dsc:true });
+});
+
 module.exports = router;
