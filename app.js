dd9ae03ad4a917b95344936506949470ffe0d4da
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-12-21 06:46:58 +0530

    vercel

--- app.js
+++ app.js
@@ -1,86 +1,86 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var hbs = require('express-handlebars');
-const multer = require('multer');
-var userRouter = require('./routes/user');
-var adminRouter = require('./routes/admin');
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var hbs = require('express-handlebars');
+// const multer = require('multer');
+// var userRouter = require('./routes/user');
+// var adminRouter = require('./routes/admin');
 
-var app = express();
-var db = require('./config/connection')
-var session = require('express-session')
-var Handlebars = require('handlebars');
+// var app = express();
+// var db = require('./config/connection')
+// var session = require('express-session')
+// var Handlebars = require('handlebars');
 
-Handlebars.registerHelper("inc", function (value, options) {
-  return parseInt(value) + 1;
-});
+// Handlebars.registerHelper("inc", function (value, options) {
+//   return parseInt(value) + 1;
+// });
 
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
+// Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+//   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+// });
 
-// view engine setup
-app.use(express.static(__dirname + '/public'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// // view engine setup
+// app.use(express.static(__dirname + '/public'));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
-app.engine('hbs', hbs.engine({
-  extname: 'hbs',
-  defaultLayout: 'layout',
-  layoutsDir: __dirname + '/views/layout/',
-  partialsDir: __dirname + '/views/partials/'
-}))
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: "Key",
-  resave: false,
-  saveUninitialized: false
-}));
-
-db.connect((err) => {
-  if (err) {
-    console.log('Error' + err);
-  } else {
-    console.log('Database Connected');
-  }
-})
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
+// app.engine('hbs', hbs.engine({
+//   extname: 'hbs',
+//   defaultLayout: 'layout',
+//   layoutsDir: __dirname + '/views/layout/',
+//   partialsDir: __dirname + '/views/partials/'
+// }))
 
 
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session({
+//   secret: "Key",
+//   resave: false,
+//   saveUninitialized: false
+// }));
 
-app.use('/', userRouter);
-app.use('/admin', adminRouter);
+// db.connect((err) => {
+//   if (err) {
+//     console.log('Error' + err);
+//   } else {
+//     console.log('Database Connected');
+//   }
+// })
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// })
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+// app.use('/', userRouter);
+// app.use('/admin', adminRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
