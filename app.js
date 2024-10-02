453a3bab307e1cbcc1e5bf73e1d446e0bd61405f
Thoufeeque Rahman <rahman.am90@gmail.com>
2024-10-02 15:58:46 +0000

    updated app.js

--- app.js
+++ app.js
@@ -67,11 +67,12 @@ db.connect((err) => {
 })
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination:(req, file, cb)=> {
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    const uniqueSuffix = Date.now().+'-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
   }
 })
 
